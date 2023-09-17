2c92a0a1544e53acaba27c699f054423dfcff37c
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2023-09-17 17:40:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,19 @@
 import requests
-import os
 
 # AUTH GITHUB_API
-access_token = os.environ.get('GITHUB_TOKEN')
-
-# REPOS VAR DECLARATION
-repos_url = "https://api.github.com/randeldarlei/repos"
-
-# MAKE REQUEST FROM GITHUB API
+token = "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"
+url = "https://api.github.com/repositories"
 headers = {
-    "Authorization": f"token {GITHUB_TOKEN}"
+    'Accept': 'application/vnd.github+json' ,
+    'Authorization': f'Bearer {token}' ,
+    'X-GitHub-Api-Version': '2022-11-28'
 }
-response = requests.get(repos_url, headers=headers)
+# Realiza a chamada de API
+response = requests.get(url, headers=headers)
 
-# VALIDATE REQUEST
 if response.status_code == 200:
-    repos = response.json()
-
-    for repo in repos:
-        repo_name = repo["Dialog-Bot"]
-        archive_url = f"https://api.github.com/repos/{repo['full_name']}/zipball"
-        
-        # GITPYTHON LIB DOWNLOAD A REPO.
-
-        print(f"Download {repo_name}...")
-        response = requests.get(archive_url, headers=headers, stream=True)
-
-        if response.status_code == 200:
-        # MAKE A ZIP FILE AND SAVE FILES
-            os.makedirs(repo_name, exist_ok=True)
-            zip_file_path = os.path.join(repo_name, f"{repo_name}.zip")
-            
-            with open(zip_file_path, 'wb') as f:
-                for chunk in response.iter_content(chunk_size=8192):
-                    f.write(chunk)
-            
-            print(f"{repo_name} download susscesfull.")
-        else:
-            print(f"Download failed {repo_name}.")
+    data =response.json()
+    for repo in data:
+        print(repo['name'])
 else:
-    print("Error cannot access GITHUB API.") 
+    print(f"Erro na chamada de API: {response.status_code} - {response.text}") 
